2ee9dce90b6145f7e0776d90424674bf27bb3af6
João Oliveira <joao.p.oliveira@ua.pt>
2022-05-31 10:47:16 +0100

    Init

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <zephyr.h>
+#include <device.h>
+#include <drivers/gpio.h>
+#include <drivers/adc.h>
+#include <string.h>
+#include <devicetree.h>
+#include <sys/printk.h>
+#include <sys/__assert.h>
+#include <timing/timing.h>
+#include <stdio.h>
+#include <sys/printk.h>
+#include <hal/nrf_saadc.h>
+
+int main(void)
+{
+    return 0;
+}
